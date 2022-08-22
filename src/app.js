1fbe358d606994dbb442d9bad278e1bf4774eed4
jesus <m6nuel@gmail.com>
2022-08-22 15:19:14 -0300

    listo para despliegue

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const routes = require('./routes/index.js');
 
 const server = express();
+const cors = require('cors');
+
+server.use(cors())
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
+server.use(cookieParser());
+server.use(morgan('dev'));
+server.use((req, res, next) => {            //http://localhost:3000 cambiamos por * para quitar el err de cors
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 server.use('/', routes);
 
